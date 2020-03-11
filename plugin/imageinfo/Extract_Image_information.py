fe9979844a85d10f6984aef3a485d92b3f788070
kim-do-hyeon <pental@system32.kr>
2020-03-11 21:07:44 +0900

    Added imageinfo plugin feature

--- plugin/imageinfo/Extract_Image_information.py
+++ plugin/imageinfo/Extract_Image_information.py
@@ -1,9 +1,13 @@
 #Written by Pental
-#Imageinfo 실행시 나오는 결과중 중요한것 추출
-print ("Source by Pental / Extract Important Imageinfo Information \n")
+
 import re
+import os
 import string
 
+path = os.path.abspath(".\plugin\init\init_info.txt")
+
+imageinfo_result = open('.\plugin\imageinfo\imageinfo_result.txt','w',-1,"utf-8")
+print ("Source by Pental / Extract Important Imageinfo Information \n", file = imageinfo_result)
 # Function
 #Find Unique Word
 def findSentence(fileName, findText):
@@ -19,58 +23,45 @@ def findSentence(fileName, findText):
                 result.append(sentence + ".")
     file.close()
     return result
-
 #Delete Special Text
 def cleanText(readData):
     text = re.sub('[(),:]','',readData)
     return text
-
 #---------------Find Image--------------
 #Find Unique Word (Win)
-result = findSentence("image.txt", "Win")
-
+result = findSentence(path, "Win")
 for sentence in result:
     extact_sentece = sentence
-
 if __name__ == "__main__":
     oriText = extact_sentece
-#    print('Del Special word : ', cleanText(oriText))
     del_speical_word_image = cleanText(oriText)
-
 #Del SpaceBar
 replace_space_bar_image = del_speical_word_image.replace(" ","")
-
 image_information = replace_space_bar_image[17:28]
-print ('Image Profile :',image_information)
-
-#--------------------------FileAddressSpace---------------------
-fileaddress = findSentence("image.txt", "FileAddressSpace")
+print ('Image Profile :',image_information, file = imageinfo_result)
+#--------------------------FilePath---------------------
+fileaddress = findSentence(path, "FileAddressSpace")
 #Del Unique Word - File Space
 for sentence in fileaddress:
     file_space = sentence
-
 if __name__ == "__main__":
     oriText = file_space
     del_speical_word_file_space = cleanText(oriText)
-
 #Del Space Bar - File Space
 replace_space_bar_file_space = del_speical_word_file_space.replace(" ","")
 i = len(replace_space_bar_file_space)
 file_space = replace_space_bar_file_space[24:i-1]
-print ('Image File Address :',file_space)
-
+print ('Image File Path :',file_space, file = imageinfo_result)
 #------------Image date and time--------------
-imagedataandtime = findSentence("image.txt", "Image date and time")
+imagedataandtime = findSentence(path, "Image date and time")
 #Del Unique Word - Image date and time
 for sentence in imagedataandtime:
     image_data_and_time = sentence
-
 if __name__ == "__main__":
     oriText = image_data_and_time
     del_speical_word_image_data_and_time = cleanText(oriText)
-
 #Del Space Bar - Image date and time
 replace_space_bar_image_data_and_time = del_speical_word_image_data_and_time.replace(" ","",12)
 i = len(replace_space_bar_image_data_and_time)
 image_data_and_time = replace_space_bar_image_data_and_time[20:i-1]
-print ('Image Data And Time :',image_data_and_time)
+print ('Image Date And Time :',image_data_and_time, file = imageinfo_result)
--- ui.py
+++ ui.py
@@ -12,11 +12,10 @@ log = open('vol_log.txt','w',-1,"utf-8")
 
 #Time Stamp
 now = datetime.now()
-time = "{}/{}/{} {}:{}:{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+time = "{}{}{}{}{}{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
 print("Init TIME : " + time, file = log)
 print("Init TIME : " + time)
 
-# print("Init TIME : " + time , file=log)
 ui = uic.loadUiType("gui.ui")[0]
 
 #Main Window
@@ -29,81 +28,140 @@ class MyWindow(QMainWindow, ui):
         #File Path Call
         self.open_file_btn.clicked.connect(self.callfile)
 
+        #Init Scan
+        self.init_scan.clicked.connect(self.Init_Scan) #Scan
+
         #Scan, Cancel Button Call
         self.scan.clicked.connect(self.scan_btn) #Scan
         self.exit.clicked.connect(self.Exit) #Exit
-        
+
         #Plugin Call
         self.imageinfo_btn.clicked.connect(self.imageinfo)
         self.cmdscan_btn.clicked.connect(self.cmdscan)
-        self.pstree_btn.clicked.connect(self.PluginButtonClicked)
+        self.pstree_btn.clicked.connect(self.pslist)
 
-        #ProgressBar Call
-        self.progressBar.valueChanged.connect(self.progress)
-    
+
     #Call File Function
     def callfile(self):
         fname = QFileDialog.getOpenFileName(self)
         self.file_path.setText(fname[0])
         path = pathlib.Path(fname[0])
-        print(time + " > [CALLFILE] Selected Image Path " + str(path), file = log)
-        print(time + " > [CALLFILE] Selected Image Path " + str(path))
+        print(" > [CALL FILE] Selected Image Path " + str(path), file = log)
+        print(" > [CALL FILE] Selected Image Path " + str(path))
         return path
 
-    #Scan Button - Message
+    #Init_Scan Button - Message
+    #Imageinfo > Initial
+    def Init_Scan(self):
+        path = self.file_path.toPlainText()
+        path = pathlib.Path(path)
+
+        print(" > [INIT SCAN] Image Scan Path " + str(path), file = log)
+        print(" > [INIT SCAN] Image Scan Path " + str(path))
+
+        self.Command.setText("Init Scanning!")
+
+        print(" > [INIT SCAN] Scanning!", file = log)
+        print(" > [INIT SCAN] Scanning!")
+
+        plugin = self.work.toPlainText()
+
+        print(" > [INIT SCAN] Imageinfo Scan", file = log)
+        print(" > [INIT SCAN] Imageinfo Scan")
+
+        #Run Volatility With Subprocess
+        shell = ['volatility_2.6_win64_standalone.exe','-f', str(path), 'imageinfo']
+        fd_open = subprocess.Popen(shell, stdout=subprocess.PIPE).stdout
+        data = fd_open.read().strip().decode('utf8')
+        fd_open.close()
+
+        imageinfo = open('plugin/init/init_info.txt','w',-1,"utf-8")
+        print (data, file = imageinfo) #init_info(imageinfo) Save
+
+        self.Command.setText("Scanned!")
+        print(" > [INIT SCAN] Scanned!", file = log)
+        print(" > [INIT SCAN] Scanned!")
+
+        self.Command_Result.setText(data)
+
+    #Scan
     def scan_btn(self):
         path = self.file_path.toPlainText()
         path = pathlib.Path(path)
-        print(time + " > [SCAN] Image Scan Path " + str(path), file = log)
-        print(time + " > [SCAN] Image Scan Path " + str(path))
+
+        print(" > [SCAN] Image Scan Path " + str(path), file = log)
+        print(" > [SCAN] Image Scan Path " + str(path))
+
         self.Command.setText("Scanning!")
-        print(time + " > [SCAN] Scanning!", file = log)
-        print(time + " > [SCAN] Scanning!")
-        subprocess.run(['volatility_2.6_win64_standalone.exe','-f', str(path),'imageinfo', '> ./imageinfo/imageinfo.txt'], shell=True, check=True)
+
+        print(" > [SCAN] Scanning!", file = log)
+        print(" > [SCAN] Scanning!")
+
+        plugin = self.work.toPlainText()
+
+        print(" > [SCAN] Plugin : " + plugin, file = log)
+        print(" > [SCAN] Plugin : " + plugin)
+
+        #Run Volatility With Subprocess
+        shell = ['volatility_2.6_win64_standalone.exe','-f', str(path), plugin]
+        fd_open = subprocess.Popen(shell, stdout=subprocess.PIPE).stdout
+        data = fd_open.read().strip().decode('utf8')
+        fd_open.close()
+
+        plugin_log = open('plugin/init/plugin.txt','w',-1,"utf-8")
+        print (data, file = plugin_log)
+
         self.Command.setText("Scanned!")
-        print(time + " > [SCAN] Scanned!", file = log)
-        print(time + " > [SCAN] Scanned!")
+        print(" > [SCAN] Scanned!", file = log)
+        print(" > [SCAN] Scanned!")
 
-    def progress(self):
-            self.progressBar.setValue(100)
-
-    #Plugin Call - Message // Soon Delete
-    def PluginButtonClicked(self):
-        msg = ""
-        if self.imageinfo_btn.isChecked():
-            msg = "Selected Imageinfo"
-            print(time + " > [IMAGEINFO] " + msg, file = log)
-        elif self.cmdscan_btn.isChecked():
-            msg = "Selected Cmdscan"
-            print(time + " > [CMDSCAN] " + msg, file = log)
-        elif self.pstree_btn.isChecked():
-            msg = "Selected Pstree"
-            print(time + " > [PSTREE] " + msg, file = log)
-        self.statusbar.showMessage(msg + "!")
-    #-------------------------------------------------------
+        self.Command_Result.setText(data)
 
     #Plugin - imageinfo
     def imageinfo(self):
-        msg = ""
-        imageinfo_result_file = open('imageinfo/image.txt','r')
+        print(" > [PLUGIN] imageinfo Selected!", file = log)
+        print(" > [PLUGIN] imageinfo Selected!")
+        self.statusbar.showMessage("Selected imageinfo!")
+        self.work.setText('imageinfo')
+
+        #File Path Set
+        pwd = os.getcwd() + "\plugin\imageinfo\Extract_Image_information.py"
+        print(" > [PLUGIN] imageinfo manage : ", pwd, file = log)
+        print(" > [PLUGIN] imageinfo manage : ", pwd)
+
+        #Call Plugin
+        subprocess.call(["python", pwd])
+
+        #Result Manage
+        imageinfo_result_file = open('plugin/imageinfo/imageinfo_result.txt','r')
         Textstring = imageinfo_result_file.read()
         self.Command_Result.setText(Textstring)
         imageinfo_result_file.close()
-        self.statusbar.showMessage("Selected imageinfo!")
+        
+#--------------------PLUGIN----------------------------------------------#
 
     #Plugin - cmdscan
     def cmdscan(self):
-        msg = ""
-        cmdscan_result_file = open('cmdscan/cmdscan.txt','r')
+        cmdscan_result_file = open('plugin/cmdscan/cmdscan.txt','r')
         Textstring = cmdscan_result_file.read()
         self.Command_Result.setText(Textstring)
         cmdscan_result_file.close()
         self.statusbar.showMessage("Selected Cmdscan!")
+        self.work.setText('cmdscan')
+
+    #Plugin - pslist
+    def pslist(self):
+        pslist_result_file = open('plugin/pslist/pslist.txt','r')
+        Textstring = pslist_result_file.read()
+        self.Command_Result.setText(Textstring)
+        pslist_result_file.close()
+        self.statusbar.showMessage("Selected pslist!")
+        self.work.setText('pslist')
 
     #Exit
     def Exit(self):
-        print(time + " > [EXIT] Exit! Bye", file = log)
-        print(time + " > [EXIT] Exit! Bye")
+        print(" > [EXIT] Exit! Bye", file = log)
+        print(" > [EXIT] Exit! Bye")
         self.exit.clicked.connect(app.quit)
 
 if __name__ == "__main__":
