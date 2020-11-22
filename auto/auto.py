91eb176877ca1bfed704e47b8161035e35831f8b
kim-do-hyeon <pental@kakao.com>
2020-11-22 14:51:14 +0900

    Add db store button & bug fix

--- auto/auto.py
+++ auto/auto.py
@@ -25,6 +25,16 @@ print(" ####### #    #   #   #    #    ####### #  # # ###### #        #     #   
 print(" #     # #    #   #   #    #    #     # #   ## #    # #        #    #     #      #   #  ")
 print(" #     #  ####    #    ####     #     # #    # #    # ######   #   ###### ###### #    # ")
 
+text = 'Volatility Auto GUI environment. Sourced By PENTAL \
+        \n \
+        \n 1. Mount the image first.\
+        \n 2. press the Analyze button.\
+        \n 3. If Process finished, Click the DB Store button to check the result and add the database frame. \
+        \n 4. Click the EXIT button to exit. \
+        \n \n \
+        Update & issue https://github.com/kim-do-hyeon/Volaltility-gui \
+        \n Thanks for your use this program. \
+        \n If possible, I would appreciate it if you hit the star button on github.'
 
 
 dir_path = os.getcwd() + '\\data'
@@ -34,25 +44,33 @@ os.mkdir(os.getcwd() + '\\data')
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(680, 45)
-        Dialog.setMinimumSize(QtCore.QSize(680, 45))
-        Dialog.setMaximumSize(QtCore.QSize(680, 45))
+        Dialog.resize(765, 220)
+        Dialog.setMinimumSize(QtCore.QSize(765, 220))
+        Dialog.setMaximumSize(QtCore.QSize(765, 220))
         self.file_path = QtWidgets.QTextBrowser(Dialog)
-        self.file_path.setGeometry(QtCore.QRect(10, 10, 421, 31))
+        self.file_path.setGeometry(QtCore.QRect(10, 10, 420, 30))
         self.file_path.setObjectName("file_path")
         self.select = QtWidgets.QPushButton(Dialog)
-        self.select.setGeometry(QtCore.QRect(440, 10, 75, 31))
+        self.select.setGeometry(QtCore.QRect(440, 10, 75, 30))
         self.select.setObjectName("select")
         self.analyze = QtWidgets.QPushButton(Dialog)
-        self.analyze.setGeometry(QtCore.QRect(520, 10, 75, 31))
+        self.analyze.setGeometry(QtCore.QRect(520, 10, 75, 30))
         self.analyze.setObjectName("analyze")
+        self.db = QtWidgets.QPushButton(Dialog)
+        self.db.setGeometry(QtCore.QRect(600, 10, 75, 30))
+        self.db.setObjectName("DB")
         self.exit = QtWidgets.QPushButton(Dialog)
-        self.exit.setGeometry(QtCore.QRect(600, 10, 75, 31))
+        self.exit.setGeometry(QtCore.QRect(680, 10, 75, 30))
         self.exit.setAcceptDrops(False)
         self.exit.setObjectName("exit")
+        self.log = QtWidgets.QTextBrowser(Dialog)
+        self.log.setGeometry(QtCore.QRect(10, 50, 745, 160))
+        self.log.setObjectName("LOG")
+
 
         self.select.clicked.connect(self.callfile)
         self.analyze.clicked.connect(self.scan)
+        self.db.clicked.connect(self.db_store)
         self.exit.clicked.connect(app.quit)
 
         self.retranslateUi(Dialog)
@@ -63,17 +81,24 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(_translate("Dialog", "Volatility Auto Analyzer"))
         self.select.setText(_translate("Dialog", "Select"))
         self.analyze.setText(_translate("Dialog", "Analyze"))
+        self.db.setText(_translate("Dialog", "DB Store"))
         self.exit.setText(_translate("Dialog", "EXIT"))
+        self.log.setText(text)
+
+    def callfile(self):
+        strFilter = "Raw file (*.raw) ;; Memory file (*.mem) ;; All files (*.*)";
+        fname = QFileDialog.getOpenFileName(filter=strFilter)
+        self.file_path.setText(fname[0])
+        path = pathlib.Path(fname[0])
+        return path
 
     def scan(self):
         path = self.file_path.toPlainText()
         path = pathlib.Path(path)
         volatility3 = os.getcwd() + "/volatility3/vol.py"
-
         f = open('plugin_list.txt', 'r', encoding='utf-8')
         plugin_list = f.read().split()
         f.close()
-
         for i in plugin_list:
             print(i)
             shell = ['python', volatility3,'-f', path, i]
@@ -86,12 +111,8 @@ class Ui_Dialog(object):
             f.close()
         print("Success")
         
-    def callfile(self):
-        strFilter = "Raw file (*.raw) ;; Memory file (*.mem) ;; All files (*.*)";
-        fname = QFileDialog.getOpenFileName(filter=strFilter)
-        self.file_path.setText(fname[0])
-        path = pathlib.Path(fname[0])
-        return path
+    def db_store(self):
+        os.system("python auto_db_store.py")
 
 if __name__ == "__main__":
     import sys
