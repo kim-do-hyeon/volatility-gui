be454d49d65dbef819029ce061692edbe401d0be
Deun Lee <54697735+deunlee@users.noreply.github.com>
2020-12-01 16:38:32 +0900

    Major UI upgrade

--- ui.py
+++ ui.py
@@ -1,24 +1,44 @@
 import os
+import re
 import sys
 import pathlib
 import subprocess
-import plugin
 from datetime import datetime
+
 from PyQt5 import uic
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-#Record Log
-log = open('log.txt','w',-1,"utf-8")
+import plugin
 
-#Time Stamp
-def timestamp(self):
-    now = datetime.now()
-    time = "{}{}{}{}{}{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second) #timestamp 형식
-    return time
+
+volatility3 = os.getcwd() + '/volatility3/vol.py'
+log_file = open('log.txt', 'w', -1, 'utf-8')
+
+def timestamp():
+    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    # now = datetime.now()
+    # time = "{}{}{}{}{}{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second) #timestamp 형식
+    # return time
+
+def log(message):
+    message = timestamp() + ' > ' + message
+    print(message, file=log_file)
+    print(message)
+
+def fix_file_name(name):
+    return re.sub('[\/:*?"<>|]', '', name)
+
+def remove_all_file(path):
+    if os.path.exists(path):
+        for file in os.scandir(path):
+            os.remove(file.path)
+
+####################################
 
 ui = uic.loadUiType("gui.ui")[0]
-text = 'Volatility GUI environment. Sourced By PENTAL \
+default_message = 'Volatility GUI environment. Sourced By PENTAL \
         \n \
         \n 1. Mount the image first.\
         \n 2. After setting the plug-in, press the scan button.\
@@ -28,113 +48,229 @@ text = 'Volatility GUI environment. Sourced By PENTAL \
         Update & issue https://github.com/kim-do-hyeon/Volaltility-gui \
         \n Thanks for your use this program. \
         \n If possible, I would appreciate it if you hit the star button on github.'
-#Main Window
+
+
 class MyWindow(QMainWindow, ui):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setWindowIcon(QIcon('icon.png'))
 
-        #Call File Path
-        self.open_file_btn.clicked.connect(self.callfile)
+        # Button
+        self.btn_image_open.clicked.connect(self.btn_image_open_click)
+        self.btn_plugin_check.clicked.connect(self.btn_plugin_check_click)
+        self.btn_plugin_uncheck.clicked.connect(self.btn_plugin_uncheck_click)
+        self.btn_scan.clicked.connect(self.btn_scan_click)
+        self.btn_exit.clicked.connect(self.btn_exit_click)
 
-        #Call Scan, Cancel Button
-        self.exit.clicked.connect(self.Exit) #Exit
-        self.scan_vol3.clicked.connect(self.scan) #Scan
+        # Text
+        self.txt_date.setText(timestamp())
+        self.txt_result.setText(default_message)
 
-        #Call Save log, Add DB
-        self.save_log_btn.clicked.connect(self.save_log)
-        self.add_db_btn.clicked.connect(self.add_db)
+        # Plugin List
+        # self.list_plugins.itemClicked.connect(self.list_plugins_item_click)
+        # self.list_plugins.itemDoubleClicked.connect(self.list_plugins_item_click)
+        for plugin_name in plugin.__all__:
+            item = QListWidgetItem()
+            item.setText(plugin_name)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Unchecked)
+            if (plugin_name in ['windows.cmdline', '1windows.filescan']): # for test
+                item.setCheckState(Qt.Checked)
+            self.list_plugins.addItem(item)
 
-        #Call Plugin
-        self.plugin_list.currentIndexChanged.connect(self.pluginlist)
+        # Thread
+        self.th_scan = ScanThread(self)
+        self.th_scan.evt_result_append.connect(self.th_scan_result_handler)
+        self.th_scan.evt_status_changed.connect(self.th_scan_status_handler)
+        self.th_scan.evt_scan_finished.connect(self.th_scan_finish_handler)
 
-        self.Command_Result.setText(text)
 
-    #Call Plugin List
-    def pluginlist(self):
-        self.work.setText(self.plugin_list.currentText())
+    def list_plugins_item_click(self) :
+        item = self.list_plugins.currentItem()
+        print(item.text())
+        if (item.checkState() == Qt.Checked):
+            item.setCheckState(Qt.Unchecked)
+        else:
+            item.setCheckState(Qt.Checked)
 
-    #Scan
-    def scan(self):
-        path = self.file_path.toPlainText()
-        path = pathlib.Path(path)
-        
-        print(timestamp(self) + " > [SCAN] Image Scan Path " + str(path), file = log)
-        print(timestamp(self) + " > [SCAN] Image Scan Path " + str(path))
-        
-        self.Command.setText("File Scanning!")
-        print(timestamp(self) + " > [SCAN] Scanning!", file = log)
-        print(timestamp(self) + " > [SCAN] Scanning!")
 
-        plugin = self.work.toPlainText() #Call Plugin Text
+    def btn_image_open_click(self):
+        file_filter = 'Raw file (*.raw) ;; Memory file (*.mem) ;; All files (*.*)'
+        file_path = QFileDialog.getOpenFileName(self, 'Select Image', filter=file_filter)
+        log('[FILE] Image Path: ' + file_path[0])
+        self.txt_image_path.setText(file_path[0])
 
-        print(timestamp(self) + " > [SCAN] Plugin : " + plugin, file = log)
-        print(timestamp(self) + " > [SCAN] Plugin : " + plugin)
-    
-        #Run Volatility With Subprocess
-        volatility3 = os.getcwd() + "/volatility3/vol.py"
-        
-        shell = ['python', volatility3,'-f', str(path), plugin]
-        fd_open = subprocess.Popen(shell, stdout=subprocess.PIPE).stdout
-        data = fd_open.read().strip().decode('euc-kr')
-        fd_open.close()
 
-        save_log_path = 'plugin/init/' + plugin + '/' + plugin + '.txt'
-        plugin_log = open(str(save_log_path),'w',-1,"utf-8")
-        print (data, file = plugin_log)
+    def btn_plugin_check_click(self):
+        for i in range(self.list_plugins.count()):
+            self.list_plugins.item(i).setCheckState(Qt.Checked)
 
-        self.Command.setText(plugin + " Scanned!")
-        print(timestamp(self) + " > [SCAN] " + plugin + "Scanned!", file = log)
-        print(timestamp(self) + " > [SCAN] " + plugin + "Scanned!")
-        data = data.replace('Volatility 3 Framework 1.0.0-beta.1','')
-        self.Command_Result.setText(data)
 
-    #Call File Function
-    def callfile(self):
-        strFilter = "Raw file (*.raw) ;; Memory file (*.mem) ;; All files (*.*)";
-        fname = QFileDialog.getOpenFileName(self, "Load Image", filter=strFilter)
-        self.file_path.setText(fname[0])
-        path = pathlib.Path(fname[0])
-        print(timestamp(self) + " > [CALL FILE] Selected Image Path " + str(path), file = log)
-        print(timestamp(self) + " > [CALL FILE] Selected Image Path " + str(path))
-        return path
+    def btn_plugin_uncheck_click(self):
+        for i in range(self.list_plugins.count()):
+            self.list_plugins.item(i).setCheckState(Qt.Unchecked)
 
-    #Add Db
+
+    def btn_scan_click(self):
+        # Image Path
+        image_path = self.txt_image_path.toPlainText()
+        if (image_path == ''):
+            QMessageBox.warning(self, 'Error', 'Please select an image.', QMessageBox.Ok, QMessageBox.Ok)
+            return
+        log('[SCAN] Image Path: ' + image_path)
+
+        # Plugin
+        selected_plugins = []
+        for i in range(self.list_plugins.count()):
+            item = self.list_plugins.item(i)
+            if (item.checkState() == Qt.Checked):
+                selected_plugins.append(item.text())
+        if len(selected_plugins) == 0:
+            QMessageBox.warning(self, 'Error', 'Please select at least one plugin.', QMessageBox.Ok, QMessageBox.Ok)
+            return
+
+        # Case Name
+        case_name = fix_file_name(self.txt_case_name.text()).strip()
+        self.txt_case_name.setText(case_name)
+        if (len(case_name) == 0):
+            QMessageBox.warning(self, 'Error', 'Please enter the case name.', QMessageBox.Ok, QMessageBox.Ok)
+            return
+
+        # Case Path
+        case_path = 'case/' + case_name
+        if not os.path.exists(case_path):
+            os.makedirs(case_path)
+        else:
+            reply = QMessageBox.question(self, 'Case Exists', 'The same case name exists.\nWould you like to overwrite it?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.No:
+                return
+            remove_all_file(case_path)
+
+        # Run Volatility
+        self.txt_result.setText('Case: ' + case_name)
+        self.txt_result.append('Image: ' + os.path.basename(image_path))
+        self.set_enabled(False)
+        self.th_scan.set_image_path(image_path)
+        self.th_scan.set_case_path(case_path)
+        self.th_scan.set_plugin_list(selected_plugins)
+        self.th_scan.start()
+
+
+    def th_scan_result_handler(self, message):
+        self.txt_result.append(message)
+
+
+    def th_scan_status_handler(self, message):
+        self.lbl_status.setText(message)
+
+
+    def th_scan_finish_handler(self):
+        self.set_enabled(True)
+        QMessageBox.information(self, 'Finished', 'Scan finished!', QMessageBox.Ok, QMessageBox.Ok)
+
+
+    def set_enabled(self, enabled):
+        self.lbl_case_name.setEnabled(enabled)
+        self.lbl_date.setEnabled(enabled)
+        self.lbl_image.setEnabled(enabled)
+        self.txt_case_name.setEnabled(enabled)
+        self.txt_date.setEnabled(enabled)
+        self.txt_image_path.setEnabled(enabled)
+        self.btn_image_open.setEnabled(enabled)
+        self.btn_plugin_check.setEnabled(enabled)
+        self.btn_plugin_uncheck.setEnabled(enabled)
+        self.btn_scan.setEnabled(enabled)
+        self.btn_exit.setEnabled(enabled)
+        self.list_plugins.setEnabled(enabled)
+
+
     def add_db(self):
         plugin = self.work.toPlainText()
         path = os.getcwd() + "/plugin/init/" + plugin + "/" + plugin + ".py"
         path = pathlib.Path(path)
-        print(timestamp(self) + " > [ADD DB] Plugin : " + plugin, file = log)
-        print(timestamp(self) + " > [ADD DB] Plugin : " + plugin)
+        print(timestamp() + " > [ADD DB] Plugin : " + plugin, file = log)
+        print(timestamp() + " > [ADD DB] Plugin : " + plugin)
         os.system('python ' + str(path))
-        print(timestamp(self) + " > [ADD DB] Added DB!", file = log)
-        print(timestamp(self) + " > [ADD DB] Added DB!")
+        print(timestamp() + " > [ADD DB] Added DB!", file = log)
+        print(timestamp() + " > [ADD DB] Added DB!")
 
 
-    #Save Log
     def save_log(self):
-        txt = self.Command_Result.toPlainText()
+        txt = self.txt_result.toPlainText()
         savefilename = QFileDialog.getSaveFileName(self, "Save File", filter="*.txt")
-        print(timestamp(self) + " > [SAVE LOG] Save Path " + savefilename[0], file = log)
-        print(timestamp(self) + " > [SAVE LOG] Save Path " + savefilename[0])
+        print(timestamp() + " > [SAVE LOG] Save Path " + savefilename[0], file = log)
+        print(timestamp() + " > [SAVE LOG] Save Path " + savefilename[0])
         if savefilename == "":
             return
             print("ERR")
         f = open(savefilename[0],'wb')
         f.write(txt.encode())
         f.close()
-        print(timestamp(self) + " > [SAVE LOG] Saved!", file = log)
-        print(timestamp(self) + " > [SAVE LOG] Saved!")
+        print(timestamp() + " > [SAVE LOG] Saved!", file = log)
+        print(timestamp() + " > [SAVE LOG] Saved!")
 
-    #Exit
-    def Exit(self):
 
-        print(timestamp(self) + " > [EXIT] Exit! Bye", file = log)
-        print(timestamp(self) + " > [EXIT] Exit! Bye")
-        self.exit.clicked.connect(app.quit)
+    def btn_exit_click(self):
+        log('[EXIT] Exit! Bye')
+        QCoreApplication.quit()
 
-#Call Ui
+
+ 
+class ScanThread(QThread):
+    evt_result_append = pyqtSignal(str)
+    evt_status_changed = pyqtSignal(str)
+    evt_scan_finished = pyqtSignal()
+ 
+    def __init__(self, parent=None):
+        super().__init__()
+        self.plugins = None
+        self.image_path = None
+        self.case_path = None
+        self.main = parent
+ 
+    def set_plugin_list(self, plugins):
+        self.plugins = plugins
+
+    def set_image_path(self, path):
+        self.image_path = path
+
+    def set_case_path(self, path):
+        self.case_path = path
+
+    def run(self):
+        start_time = timestamp()
+
+        for plugin_name in self.plugins:
+            shell = ['python', volatility3, '-f', self.image_path, plugin_name]
+            log('[SCAN] Current Plugin: ' + plugin_name)
+            log('[SCAN] Run: ' + ' '.join(shell))
+            self.evt_status_changed.emit('Scanning: ' + plugin_name)
+
+            self.evt_result_append.emit('\n\n' + '=' * 80)
+            self.evt_result_append.emit(plugin_name)
+            self.evt_result_append.emit('=' * 80 + '\n')
+
+            process = subprocess.Popen(shell, stdout=subprocess.PIPE).stdout
+            result = process.read().strip().decode('euc-kr')
+            result = result.replace('\r', '')
+            result = result.split('\n', 2)[2]
+            process.close()
+
+            save_path = self.case_path + '/' + plugin_name + '.txt'
+            plugin_log = open(save_path, 'w', -1, 'utf-8')
+            print(result, file=plugin_log)
+
+            self.evt_result_append.emit(result)
+        
+        self.evt_result_append.emit('\n' + '=' * 50)
+        self.evt_result_append.emit('Scan started at ' + start_time)
+        self.evt_result_append.emit('Scan finished at ' + timestamp() + '\n')
+
+        self.evt_status_changed.emit('SCAN FINISHED!')
+        self.evt_scan_finished.emit()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
